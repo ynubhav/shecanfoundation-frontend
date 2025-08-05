d56faea42fd2817934f516b7cfe4b256e562f8be
ynubhav <anubhavdixit35@gmail.com>
2025-08-06 00:25:37 +0530

    Added responsiveness with device

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="bg-slate-950 text-gray-300 pt-16 pb-6 px-4 mt-2 w-1/1 ">
+    <div className="bg-slate-950 text-gray-300 pt-16 pb-6 px-4 mt-2">
       <p className="text-center italic text-sm font-semibold">SheCanFoundation@2025</p>
     </div>
   );
