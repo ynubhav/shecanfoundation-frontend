77ddd523d81f814d8bb5756a5821ccdbcbf2945a
ynubhav <anubhavdixit35@gmail.com>
2025-08-05 19:23:40 +0530

    .

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,7 @@
 export default function Footer() {
   return (
-    <div className="bg-slate-950 text-gray-300 pt-4 px-4 mt-2 w-1/1 ">
-      <p className="text-center text-sm font-semibold">SheCanFoundation@2025</p>
-      <p className="text-center text-sm p-1">Licensed Under MIT</p>
+    <div className="bg-slate-950 text-gray-300 pt-16 pb-6 px-4 mt-2 w-1/1 ">
+      <p className="text-center italic text-sm font-semibold">SheCanFoundation@2025</p>
     </div>
   );
 }
